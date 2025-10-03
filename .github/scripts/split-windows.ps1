2a14f7132dca54a1720ef5af51c7b3d4e6c79a57
max <you@example.com>
2025-10-03 20:43:23 +0200

    Fix variable naming and improve file writing logic in Windows artifact splitting script

--- .github/scripts/split-windows.ps1
+++ .github/scripts/split-windows.ps1
@@ -1,15 +1,17 @@
 $inputFile = "ai-image-tagger-windows.zip"
 $chunkSize = 1500MB
-$stream = [System.IO.File]::OpenRead($inputFile)
+$inputStream = [System.IO.File]::OpenRead($inputFile)
 $buffer = New-Object byte[] $chunkSize
 $partNum = 0
 
-while (($bytesRead = $stream.Read($buffer, 0, $buffer.Length)) -gt 0) {
+while (($bytesRead = $inputStream.Read($buffer, 0, $buffer.Length)) -gt 0) {
     $outputFile = "ai-image-tagger-windows.zip.part-" + ([string]$partNum).PadLeft(2, '0')
-    [System.IO.File]::WriteAllBytes($outputFile, $buffer[0..($bytesRead-1)])
+    $outputStream = [System.IO.File]::OpenWrite($outputFile)
+    $outputStream.Write($buffer, 0, $bytesRead)
+    $outputStream.Close()
     Write-Host "Created $outputFile"
     $partNum++
 }
 
-$stream.Close()
+$inputStream.Close()
 Write-Host "Split into $partNum parts"
